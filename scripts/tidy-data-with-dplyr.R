b025654bbbaac4b38b34535f7d264bf1c6332850
avanlinden <11965371+avanlinden@users.noreply.github.com>
2020-03-12 19:40:26 -0400

    delete unused basics script; add data directory

---
+++ scripts/tidy-data-with-dplyr.R
@@ -0,0 +1,12 @@
+
+### The Tidyverse is Your Friend! ###
+### UMass Data Viz in R 3-13-2020 ###
+### Abby Vander Linden ###
+
+library(tidyverse)
+
+### Dplyr and tibbles ====
+
+# Dplyr is an R package that provides a flexible grammar for manipulation of dataframes
+# We won't actually use any standard R dataframes here -- we will work with data as 'tibbles', or dplyr's version of tables
+
