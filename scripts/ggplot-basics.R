76af9a78de8fc1ec2d65c9425f3225c3c9507b91
avanlinden <11965371+avanlinden@users.noreply.github.com>
2020-03-12 20:04:09 -0400

    topic headers

---
+++ scripts/ggplot-basics.R
@@ -0,0 +1,32 @@
+
+### Graphics Wizardry with ggplot2 ###
+### UMass Data Viz in R 3-13-2020 ###
+### Abby Vander Linden ###
+
+library(tidyverse)
+
+### Data + aesthetic mapping + geometry ====
+
+
+### 1D Continuous: Histograms ====
+
+### 2D Continuous: points, lines ====
+
+### 2D Discrete x, Continuous y: boxplot, bar plot ====
+
+### Stats: visualize data transformation ====
+
+### Faceting ====
+
+### Color and shape scales ====
+
+### Labels and text ====
+
+### Themes ====
+
+### Color palettes and other fun stuff ====
+
+
+### Heatmaps ====
+
+### Genomic data ===
