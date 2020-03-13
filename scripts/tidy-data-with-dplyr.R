8c2dc8dba34b26973e5fceac4e01fb8de1012aea
avanlinden <11965371+avanlinden@users.noreply.github.com>
2020-03-12 20:57:51 -0400

    add example code for data manipulation

--- scripts/tidy-data-with-dplyr.R
+++ scripts/tidy-data-with-dplyr.R
@@ -13,6 +13,8 @@ library(tidyverse)
 # let's look at the mpg dataset, which is part of the tidyverse and is formatted as a tibble
 mpg
 
+### Mutate, select, filter, summarise, arrange ====
+
 #we can easily filter the data by row -- let's say we want to look at cars that get only 30mpg or more on the highway
 
 #first look at the filter command
@@ -57,8 +59,65 @@ summarise(group_by(mpg, manufacturer), mean=mean(hwy))
 
 ### Load external data with readr ====
 
-### Mutate, select, filter, summarise, arrange ====
+#readr is a flexible tidyverse package for reading in rectangular data in many different types and automatically formatting it in a tibble; more info:
+?readr
+
+#let's read in a dataframe of mammal vertebrate measures I have
+
+verts <- read_csv("data/ruminant_vert_morph_data.csv") 
+
+# let's take a look!
+verts
+
+#because this csv had a column of row numbers, we have a weird column 'X1' at the start of the dataframe. We can get rid of it with select(-X1), and then store the result as a new object
+
+vertData <- select(verts, -X1)
+vertData
+
+#we can filter and arrange this dataframe, too
+#let's look at all measurements that belong to the genus "Ovis", aka sheep
+
+filter(vertData, str_detect(spp, "Ovis")) #str_detect is part of the stringr package and has nifty functions for working with character data!
+
+### Pipe operator ====
+
+#the pipe operator is a really handy tool for data manipulation and plotting in R
+# it's basically say 'take this, AND THEN do this, AND THEN do something else'
+# it's %>% , which you can get with Cmd+Shift+M
+
+#so, take our vertData AND THEN filter it so we have just measurements from vertebra C2
+
+vertData %>% 
+  filter(vertebra == 'C2') 
+
+#it's really helpful for stringing together commands, and it allows you to trouble shoot each piece without ever overwriting the original tibble
+
+vertData %>% 
+  filter(vertebra == 'C2') %>%  #filter only rows from vertebra C2
+  mutate(valueInMeters = value*0.01) %>%   #create a new column with the measurements in meters
+  group_by(sex) %>% #group males and females
+  summarise(mean = mean(valueInMeters)) #look at mean measurment values for males and females
+
+#the original tibble is unchanged!
+vertData
 
 ### Gather and spread ====
 
-### Pipe operator ====
+#These functions are often really hand with plotting -- they let you go from a wide data frame to a narrow one and vice versa
+
+#spread out our data so that each different anatomical measurement on C2 has its own column
+vertData %>% 
+  filter(vertebra == 'C2') %>% 
+  select(catNum, vertebra, measure, value) %>%  #select a few columns
+  spread(key = measure, value = value) #spread the data out using the options for "measure" as the column headers, and the "value" as the values
+  
+#if this looks good, we can store it as a new object
+wideData <- vertData %>% 
+  filter(vertebra == 'C2') %>% 
+  select(catNum, vertebra, measure, value) %>% 
+  spread(key = measure, value = value)
+
+# we can also collapse it and make it narrow using 'gather'
+
+wideData %>% 
+  gather(CH:TPLA, key = 'measure', value = 'value') #back to a narrow dataframe
